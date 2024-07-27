820322f4e63828e02389c05b434ed9e2278b79f7
AbeedRahh <abeedrahmangithub@gmail.com>
2024-07-27 20:57:43 +0530

    second commit icon

--- empapp/src/components/Navbar.jsx
+++ empapp/src/components/Navbar.jsx
@@ -1,6 +1,9 @@
 import { AppBar, Box, Button, IconButton, Toolbar, Typography } from '@mui/material'
 import React from 'react'
 import { Link } from 'react-router-dom'
+import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
+import CurrencyRupeeIcon from '@mui/icons-material/CurrencyRupee';
+import WorkIcon from '@mui/icons-material/Work';
 
 const Navbar = () => {
   return (
@@ -11,9 +14,11 @@ const Navbar = () => {
          
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Employee App
+            <AccountBalanceWalletIcon style={{fontSize:"40px"}}></AccountBalanceWalletIcon>
           </Typography>
         <Button variant='contained'>
-        <Link to ={'/'}>Add</Link> </Button>
+        <Link to ={'/'}>Add</Link> 
+         <WorkIcon style={{fontSize:"40px"}}></WorkIcon> </Button>
         <Button variant='contained'>
         <Link to ={'/v'}>View</Link> </Button>
         
